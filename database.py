1a83cf6de184e56c4abc9f8eb84bec577f5ccfa8
kazuki <k-sato>
2025-04-03 19:13:49 +0900

    ポケモンアプリ完成

---
+++ database.py
@@ -0,0 +1,97 @@
+import sqlite3
+
+def get_connection_db():
+    conn = sqlite3.connect('pokemon.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_table():
+    conn = get_connection_db()
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS pokemon(
+        id INTEGER PRIMARY KEY,
+
+        name TEXT,
+
+        type1 TEXT,
+
+        type2 TEXT,
+
+        height REAL,
+
+        weight REAL,
+
+        generation INTEGER,
+
+        total_stats INTEGER,
+
+        hp INTEGER,
+
+        attack INTEGER,
+
+        defense INTEGER,
+
+        special_attack INTEGER,
+
+        special_defense INTEGER,
+
+        speed INTEGER,
+
+        catch_rate INTEGER,
+
+        is_baby BOOLEAN,
+
+        is_legendary BOOLEAN,
+
+        evolves_from INTEGER,
+
+        image_url TEXT
+        )
+            ''')
+    conn.commit()
+    c.close()
+
+def get_pokemons():
+    conn = get_connection_db()
+    c = conn.cursor()
+    pokemons = c.execute('SELECT * FROM pokemon').fetchall()
+    c.close()
+    return pokemons
+
+
+def random_pokemons_limit(num):
+    conn = get_connection_db()
+    c = conn.cursor()
+    pokemons = c.execute('SELECT * FROM pokemon ORDER BY RANDOM() LIMIT ?', (num,)).fetchall()
+    return pokemons
+
+def show_pokemon(id):
+    conn = get_connection_db()
+    c = conn.cursor()
+    pokemon = c.execute('SELECT * FROM pokemon WHERE id = ?', (id,)).fetchone()
+    c.close()
+    return pokemon
+
+def search_pokemon(name):
+    conn = get_connection_db()
+    c = conn.cursor()
+    pokemon = c.execute('SELECT * FROM pokemon WHERE name = ?', (name,)).fetchone()
+    c.close()
+    return pokemon
+
+# 指定されたポケモンの検索回数を増加させる関数
+def increment_count(id):
+    conn = get_connection_db()
+    c = conn.cursor()
+    c.execute('UPDATE pokemon SET count = count + 1 WHERE id = ?', (id,))
+    conn.commit()
+    c.close
+
+def get_count():
+    
+    conn = get_connection_db()
+    c = conn.cursor()
+    rankings = c.execute('SELECT name, count FROM pokemon ORDER BY count DESC LIMIT 10').fetchall()
+    c.close
+    return rankings
---
+++ pokemon.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Response, jsonify
+from database import get_pokemons, random_pokemons_limit, show_pokemon,search_pokemon,increment_count, get_count
+import secrets
+import matplotlib.pyplot as plt
+import matplotlib, japanize_matplotlib
+from io import BytesIO
+
+matplotlib.use('Agg')
+
+app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(32)
+
+
+@app.route('/')
+def index():
+    # pokemons = get_pokemons()
+    pokemons = random_pokemons_limit(20)
+
+    return render_template("index.html", pokemons=pokemons)
+
+@app.route('/show/<int:id>')
+def show(id):
+    pokemon = show_pokemon(id)
+    return render_template('show.html', pokemon=pokemon)
+
+@app.route('/search')
+def search():
+    name = request.args['name']
+    # print(name)
+    pokemon = search_pokemon(name)
+    increment_count(pokemon['id'])
+    return render_template('show.html', pokemon=pokemon)
+
+# @app.route('/ranking')
+# def ranking():
+#     rankings = get_count()
+
+#     pokemon_names = []
+#     pokemon_count = []
+
+#     for ranking in rankings:
+#         pokemon_names.append(ranking["name"])
+    
+#     for ranking in rankings:
+#         pokemon_count.append(ranking["count"])
+    
+#     plt.figure(figsize=(10,6))
+#     plt.bar(pokemon_names, pokemon_count, color='skyblue')
+#     plt.title('ポケモン検索ランキング')
+#     plt.xlabel('ポケモン名')
+#     plt.ylabel('検索回数')
+#     plt.xticks(rotation=45, ha='right')
+#     plt.tight_layout()
+
+#     img = BytesIO()
+#     plt.savefig(img,format='png')
+#     img.seek(0)
+#     plt.close
+
+#     return Response(img,mimetype='image/png')
+
+@app.route('/ranking')
+def ranking():
+    rankings = get_count()
+    return jsonify([{'name': ranking['name'], 'count': ranking['count']} for ranking in rankings])
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
